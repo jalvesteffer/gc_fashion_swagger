5b0279e6e2b7ecaa56ab7bd962e4e72b759bbb1b
varghgeorge <varghgeorge@gmail.com>
2016-03-10 13:02:29 -0600

    Updated files for single list

--- src/main/java/doc/MsDocumentationApplication.java
+++ src/main/java/doc/MsDocumentationApplication.java
@@ -3,16 +3,16 @@ package doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@ComponentScan(excludeFilters = @ComponentScan.Filter(value = springfox.documentation.swagger.web.ApiResourceController.class,
-													  type = FilterType.ASSIGNABLE_TYPE))
+@ComponentScan
 @EnableAutoConfiguration
 @EnableSwagger2
+@EnableDiscoveryClient
 public class MsDocumentationApplication {
 
 	public static void main(String[] args) {
