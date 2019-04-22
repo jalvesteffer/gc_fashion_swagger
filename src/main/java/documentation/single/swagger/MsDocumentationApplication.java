3767c62c870e79a48c4d8ad9f91f776d908df70d
gvarghese <george.varghese@transplace.com>
2019-04-22 09:18:13 -0500

    Updates

--- src/main/java/documentation/single/swagger/MsDocumentationApplication.java
+++ src/main/java/documentation/single/swagger/MsDocumentationApplication.java
@@ -1,11 +1,9 @@
 package documentation.single.swagger;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
